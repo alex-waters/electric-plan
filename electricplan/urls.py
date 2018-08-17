9ddce7a4912531380c455d7239ff2ff9412971d5
alex-waters <alexwaters613@gmail.com>
2018-08-17 19:53:15 +0100

    carbon app working with no formatting

---
+++ electricplan/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.page_list, name='page_list'),
+]
--- electricplan/views.py
+++ electricplan/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import CarbonData
 
-# Create your views here.
+def page_list(request):
+    pages = CarbonData.objects.filter()
+    fcast_display = CarbonData.fcast_display
+    return render(request, 'electricplan/page_list.html', {'pages': pages, 'fcast': fcast_display})
