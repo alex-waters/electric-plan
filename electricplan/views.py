92ec79692591fd1594fa88ebc04d92dd20074db8
alex-waters <alexwaters613@gmail.com>
2018-08-24 18:26:21 +0100

    new branch that refreshes data but using a module rather than following django docs

--- electricplan/views.py
+++ electricplan/views.py
@@ -1,7 +1,24 @@
 from django.shortcuts import render
 from .models import CarbonData
+from carbondata.api_call import GridData
+
+
 
 def page_list(request):
     pages = CarbonData.objects.filter()
-    fcast_display = CarbonData.fcast_display
-    return render(request, 'electricplan/page_list.html', {'pages': pages, 'fcast': fcast_display})
+    data_refresh = GridData(15)
+    now = data_refresh.now()
+    fcast_display = data_refresh.future()
+
+    return render(
+        request,
+        'electricplan/page_list.html',
+        {
+            'pages': pages,
+            'time': now[0],
+            'intensity': now[1],
+            'fuel': now[2],
+            'fuel_pc': now[3],
+            'fcast': fcast_display
+        }
+    )
