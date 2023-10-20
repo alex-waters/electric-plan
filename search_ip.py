87036cb336a63c8410dd01fb8f0a19415990f762
Alex.Waters <alex.waters@coveainsurance.co.uk>
2023-10-20 16:52:32 +0100

    syntax fix for search_ip.py

--- search_ip.py
+++ search_ip.py
@@ -10,11 +10,11 @@ with open('/var/log/anw.pythonanywhere.com.access.log') as access_log:
         event_date = datetime.datetime.strptime(event_date_str[0], '%d/%b/%Y')
         if datetime.date.today() - event_date.date() < datetime.timedelta(8):
             found_ips.append(
-                re.findall(
+                [re.findall(
                     "[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}",
                     event
                     )[0],
-                datetime.datetime.strftime(event_date, '%A    %d %B')
+                datetime.datetime.strftime(event_date, '%A    %d %B')]
                 )
     access_log.close()
 
@@ -23,7 +23,7 @@ search_ips = set(found_ips)
 # get ip info from service and write to local file
 accessor = []
 for i in search_ips:
-    ip_lookup = requests.get(f'https://ip.rootnet.in/lookup/{i}').json()
+    ip_lookup = requests.get(f'https://ip.rootnet.in/lookup/{i[0]}').json()
     accessor.append(ip_lookup['ip'] + ' ' + ip_lookup['as']['name'])
 with open('/home/anw/mysite/electric-plan/static/ips.txt', 'w') as clear_ips:
     clear_ips.write('IPs: \n')
